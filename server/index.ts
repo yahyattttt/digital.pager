b1e11dd9271deb5fe85c9f76ac906b301ec58a90
yahyattttt <yahiatohary@hotmail.com>
2026-03-12 14:37:08 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
 
 (async () => {
   await registerRoutes(httpServer, app);
-
+serveStatic(app);
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
