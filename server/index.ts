f72fff36c1de3d9ed59e5b62e1874303e4a7e7a8
yahyattttt <yahiatohary@hotmail.com>
2026-03-13 18:23:55 +0000

    fix server static routing

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,6 @@ app.use((req, res, next) => {
 
 (async () => {
   await registerRoutes(httpServer, app);
-serveStatic(app);
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
--- server/static.ts
+++ server/static.ts
@@ -3,7 +3,8 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(process.cwd(), "dist/public");
+  const distPath = path.join(process.cwd(), "dist/public");
+
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
@@ -12,8 +13,7 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
-  app.use("/{*path}", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+  app.get("/{*path}", (_req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
   });
 }
